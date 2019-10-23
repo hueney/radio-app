ce019a7a3969e1a91771be93540bc12c6401c005
abrekos <54232528+abrekos@users.noreply.github.com>
2019-10-23 17:46:00 +0500

    Add files via upload

---
+++ banners.js
@@ -0,0 +1,19 @@
+window.Banners = [
+	{
+		title: 'Наше сообщество',
+		text: 'Подпишитесь, будьте в курсе новинок',
+		category: 'Сообщество',
+		image: '/images/banners/group.jpg',
+		type: 'link',
+		link: 'https://vk.com/radio_app'
+	},
+
+	{
+		title: 'Любимой станции нет в списке?',
+		text: 'Напишите нам!',
+		category: 'Возможности',
+		image: '/images/banners/suggestion.jpg',
+		type: 'link',
+		link: 'https://vk.me/radio_app'
+	}
+];
